2d1f40b58df63585424c4513534cc8c92e4f82a7
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-02 23:52:30 -0600

    New Version of Solution of LeetCode Problem 2506

--- Easy/Count Pairs Of Similar Strings.py
+++ Easy/Count Pairs Of Similar Strings.py
@@ -1,3 +1,4 @@
+# version 1 - Faster
 def similarPairs(words: list[str]) -> int:
     similar_pairs = 0
     for i in range(len(words)):
@@ -6,6 +7,15 @@ def similarPairs(words: list[str]) -> int:
             word2 = words[j]
             if set(word1) == set(word2): similar_pairs += 1
     return similar_pairs
+
+# version 2
+def similarPairs1(words: list[str]) -> int:
+    cnt = 0
+    # enumerate => (0, words[0]), (1, words[1])...
+    for i,w1 in enumerate(words):
+        for w2 in words[i+1:]:
+            cnt += set(w1) == set(w2)
+    return cnt
 
 words = ["aabb","ab","ba"]
 print(f"Output: {similarPairs(words)}")
