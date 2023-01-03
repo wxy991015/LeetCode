4c297431cb4cff12537bf31c82982b506d63ecad
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-02 19:39:11 -0600

    New version of solution

--- Easy/Armstrong Number.py
+++ Easy/Armstrong Number.py
@@ -1,3 +1,6 @@
+import math
+
+# version 1
 def isArmstrong(n: int) -> bool:
     str_n = str(n)
     digit_sum = 0
@@ -5,5 +8,17 @@ def isArmstrong(n: int) -> bool:
         digit_sum += int(i) ** len(str_n)
     return digit_sum == n
 
+# version 2 - Faster
+def isArmstrong1(n: int) -> bool:
+    digit_sum = 0
+    n_copy = n
+    # calculate the digit by using math module
+    k = math.floor(math.log10(n)) + 1
+    while n_copy:
+        digit = n_copy % 10
+        digit_sum += digit ** k
+        n_copy //= 10
+    return digit_sum == n
+
 n = 123
-print(f"Output: {isArmstrong(n)}")
+print(f"Output: {isArmstrong1(n)}")
