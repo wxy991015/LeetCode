bac2c769a3bf74877846860a3703113d631ff08d
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-03 22:32:47 -0600

    updated solution of LeetCode Problem 1474

--- Easy/Delete N Nodes After M Nodes of a Linked List.py
+++ Easy/Delete N Nodes After M Nodes of a Linked List.py
@@ -3,19 +3,46 @@ class ListNode:
         self.val = val
         self.next = next
 
+# version 1
 def deleteNodes(head: ListNode, m: int, n: int) -> ListNode:
-    head_copy = head
-    p = head_copy
-    m_count, n_count = 1, 0
-    while head_copy:
-        if m_count != m:
-            m_count += 1
-            head_copy = head_copy.next
-            p = p.next
+    p = ListNode()
+    modified_list = p
+    first_m, next_n = 0, 0
+    while head:
+        if first_m != m:
+            p.next = ListNode(head.val)
+            first_m += 1
         else:
-            while n_count != n:
-                n_count += 1
-                head_copy = head_copy.next
-            p.next = head_copy.next
-    return head
+            while next_n != n and head.next:
+                head = head.next
+                next_n += 1
+            # make sure that the loop stops because of next_n == n instead of head.next == None
+            if next_n == n:
+                p.next = ListNode(head.val)
+            first_m, next_n = 1, 0
+        p = p.next
+        head = head.next
+    modified_list = modified_list.next
+    return modified_list
 
+# version 2 - Faster
+# modify the original linked list instead of creating a new list
+def deleteNodes(head: ListNode, m: int, n: int) -> ListNode:
+    # define a new linked list p which adds a prev for head
+    # p.val = 0, p.next = head
+    p = ListNode(0, head)
+    first_m = 0
+    while head:
+        if first_m < m - 1:
+            first_m += 1
+        else:
+            next_n = 0
+            while next_n < n and head.next:
+                # delete node one by one
+                # 1 -> 2 -> 3 -> 4 ==> 1.next = 1.next.next = 3 ==> ListNode(2) get deleted
+                head.next = head.next.next
+                next_n += 1
+            first_m = 0
+        head = head.next
+    p = p.next
+    return p
