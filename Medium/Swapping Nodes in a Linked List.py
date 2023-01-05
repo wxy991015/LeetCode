d0ca99433bd6dd88f6a7f3a3df76f1371ed96bc1
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-04 21:55:22 -0600

    Solution of LeetCode Problem 1721

--- Medium/Swapping Nodes in a Linked List.py
+++ Medium/Swapping Nodes in a Linked List.py
@@ -5,6 +5,7 @@ class ListNode:
         self.val = val
         self.next = next
 
+# version 1
 def swapNodes(head: Optional[ListNode], k: int) -> Optional[ListNode]:
     size = 0
     p, end, swap_result = head, head, head
@@ -12,7 +13,32 @@ def swapNodes(head: Optional[ListNode], k: int) -> Optional[ListNode]:
         size += 1
         head = head.next
     beginning_k, end_k = k, size - k
-    i = 1
+    i, j = 1, 0
     head = p
-    while 
+    while i != beginning_k:
+        head = head.next
+    while j != end_k:
+        end = end.next
+    temp = head.val
+    head.val = end.val
+    end.val = temp
+    return swap_result
+
+# version 2
+def swapNodes1(head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    size = 0
+    p, end, swap_result = head, head, head
+    while head:
+        size += 1
+        head = head.next
+    beginning_k, end_k = k, size - k
+    i, j = 1, 0
+    head = p
+    while i != beginning_k:
+        head = head.next
+    while j != end_k:
+        end = end.next
+    temp = head.val
+    head.val = end.val
+    end.val = temp
     return swap_result
