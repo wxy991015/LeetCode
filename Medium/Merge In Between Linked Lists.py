3634e517de7d8a5b2a00aaf50ba49e1d6b651ad2
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-06 17:18:03 -0600

    Updated solution of LeetCode Problem 1669

--- Medium/Merge In Between Linked Lists.py
+++ Medium/Merge In Between Linked Lists.py
@@ -26,3 +26,26 @@ def mergeInBetween(list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode
     start.next = end.next
     return list1
 
+# version 2
+def mergeInBetween1(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+    curr=list1
+    t=0
+    var=None
+    while curr!=None:
+        t+=1
+        if t==b+1:
+            break
+        if a==t:
+            pp=curr.next
+            var=curr
+            var.next=list2
+            # curr.next=None
+            curr=pp
+        curr=curr.next
+    temp=list1
+    while temp!=None:
+        if temp.next==None:
+            break
+        temp=temp.next
+    temp.next=curr
+    return list1
