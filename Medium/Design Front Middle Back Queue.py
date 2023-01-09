8683efac1d7f5268e0faedf1d80ea70d20b4d19b
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-08 21:09:04 -0600

    create new Python file

---
+++ Medium/Design Front Middle Back Queue.py
@@ -0,0 +1,22 @@
+class FrontMiddleBackQueue:
+
+    def __init__(self):
+        pass
+
+    def pushFront(self, val: int) -> None:
+        pass
+
+    def pushMiddle(self, val: int) -> None:
+        pass
+
+    def pushBack(self, val: int) -> None:
+        pass
+
+    def popFront(self) -> int:
+        pass
+
+    def popMiddle(self) -> int:
+        pass
+
+    def popBack(self) -> int:
+        pass
