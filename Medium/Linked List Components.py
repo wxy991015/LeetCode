d1903aea2befdcc12a95435d83287ee154268d24
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-08 21:01:28 -0600

    Create new Python files

---
+++ Medium/Linked List Components.py
@@ -0,0 +1,10 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def numComponents(head: Optional[ListNode], nums: list[int]) -> int:
+    connected_components = 0
+    return connected_components
