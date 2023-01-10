89f8492bcfdd2f31123a603f48e1be3f6326d338
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-10 15:46:50 -0600

    Update Solution of LeetCode Problem 148

--- Medium/Sort List.py
+++ Medium/Sort List.py
@@ -5,5 +5,21 @@ class ListNode:
         self.val = val
         self.next = next
 
+# version 1
 def sortList(head: Optional[ListNode]) -> Optional[ListNode]:
-    return head
+    # do not forget to check whether head is empty
+    if head == None:
+        return head
+    list_nodes = []
+    while head:
+        list_nodes.append(head.val)
+        head = head.next
+    list_nodes = sorted(list_nodes)
+    p = ListNode()
+    result = p
+    for i in range(len(list_nodes)):
+        p.next = ListNode(list_nodes[i])
+        p = p.next
+    return result.next
+
+# version 2
