086a067007685d2ba909357e3d27c78a15ffec10
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-10 15:31:36 -0600

    Update solution of LeetCode Problem 2058

--- Medium/Find the Minimum and Maximum Number of Nodes Between Critical Points.py
+++ Medium/Find the Minimum and Maximum Number of Nodes Between Critical Points.py
@@ -7,4 +7,22 @@ class ListNode:
 
 def nodesBetweenCriticalPoints(head: Optional[ListNode]) -> list[int]:
     result = [-1, -1]
-    return result
+    critical_points = []
+    current = head
+    p = current.next
+    index = 1
+    while p.next:
+        if (current.val < p.val and p.next.val < p.val) or (current.val > p.val and p.next.val > p.val):
+            critical_points.append(index)
+        index += 1
+        current = current.next
+        p = p.next
+    if len(critical_points) < 2:
+        return result
+    minDistance = maxDistance = critical_points[1] - critical_points[0]
+    for i in range(2, len(critical_points)):
+        if critical_points[i] - critical_points[i-1] < minDistance:
+            minDistance = critical_points[i] - critical_points[i-1]
+        elif critical_points[i] - critical_points[i-1] > maxDistance:
+            maxDistance = critical_points[i] - critical_points[i-1]
+    return [minDistance, maxDistance]
