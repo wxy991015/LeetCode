bb92bd8629ba3ac03516e3905f8fb733dcc521e6
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-10 22:17:22 -0600

    Update solution of LeetCode Problem 1670

--- Medium/Design Front Middle Back Queue.py
+++ Medium/Design Front Middle Back Queue.py
@@ -1,22 +1,31 @@
 class FrontMiddleBackQueue:
-
     def __init__(self):
-        pass
+        self.fmb = []
 
     def pushFront(self, val: int) -> None:
-        pass
+        self.fmb.insert(0, val)
 
     def pushMiddle(self, val: int) -> None:
-        pass
+        middle = len(self.fmb) // 2
+        self.fmb.insert(middle, val)
 
     def pushBack(self, val: int) -> None:
-        pass
+        self.fmb.append(val)
 
     def popFront(self) -> int:
-        pass
+        if len(self.fmb) == 0:
+            return -1
+        return self.fmb.pop(0)
 
     def popMiddle(self) -> int:
-        pass
+        if len(self.fmb) == 0:
+            return -1
+        middle = len(self.fmb) // 2
+        if len(self.fmb) % 2 == 0:
+            middle -= 1
+        return self.fmb.pop(middle)
 
     def popBack(self) -> int:
-        pass
+        if len(self.fmb) == 0:
+            return -1
+        return self.fmb.pop()
