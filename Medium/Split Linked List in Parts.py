673833628254b1a8e25dfaccbc6db74b6317dce4
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-10 23:24:26 -0600

    Update Solution of LeetCode Problem 725

--- Medium/Split Linked List in Parts.py
+++ Medium/Split Linked List in Parts.py
@@ -15,9 +15,8 @@ def splitListToParts(head: Optional[ListNode], k: int) -> List[Optional[ListNode
     while p:
         nodes += 1
         p = p.next
-    print(nodes)
-    num_components = math.ceil(nodes/k)
-    print(num_components)
+    num_components = nodes // k
+    components_remain = nodes % k
     current = head
     while current:
         temp = ListNode()
@@ -28,7 +27,11 @@ def splitListToParts(head: Optional[ListNode], k: int) -> List[Optional[ListNode
             count += 1
             current = current.next
             temp = temp.next
-        p = temp.next
+        if components_remain != 0:
+            temp.next = ListNode(current.val)
+            temp = temp.next
+            components_remain -= 1
+        p = p.next
         k_parts_result[current_index] = p
         current_index += 1
     return k_parts_result
