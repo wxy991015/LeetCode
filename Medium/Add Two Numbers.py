f0d831c45806a788ed965be3422646509bc3f720
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-14 16:58:37 -0600

    update solution of LeetCode Problem 2

--- Medium/Add Two Numbers.py
+++ Medium/Add Two Numbers.py
@@ -8,13 +8,47 @@ class ListNode:
 def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
     result = ListNode()
     p = result
-    digit_sum = 0
-    while l1 and l2:
-        temp_sum = l1.val + l2.val
-        
-        if temp_sum < 10:
-            digit_sum += temp_sum
-        else:
-            pass
+    l1_num, l2_num = [], []
+    while l1:
+        l1_num.append(l1.val)
         l1 = l1.next
+    while l2:
+        l2_num.append(l2.val)
         l2 = l2.next
+    result_num = [0] * (max(len(l1_num), len(l2_num)) + 1)
+    current = 0
+    while current < len(l1_num) and current < len(l2_num):
+        val = l1_num[current] + l2_num[current]
+        if val < 10:
+            result_num[current] += val
+            if result_num[current] >= 10:
+                result_num[current] -= 10
+                result_num[current+1] += 1
+        else:
+            result_num[current] += val - 10
+            result_num[current+1] += 1
+        current += 1
+    if current < len(l1_num):
+        while current < len(l1_num):
+            val = result_num[current] + l1_num[current]
+            if val < 10:
+                result_num[current] = val
+            else:
+                result_num[current] = val - 10
+                result_num[current+1] += 1
+            current += 1
+    if current < len(l2_num):
+        while current < len(l2_num):
+            val = result_num[current] + l2_num[current]
+            if val < 10:
+                result_num[current] = val
+            else:
+                result_num[current] = val - 10
+                result_num[current+1] += 1
+            current += 1
+    if result_num[-1] == 0:
+        result_num.pop()
+    for i in result_num:
+        p.next = ListNode(i)
+        p = p.next
+    return result.next
