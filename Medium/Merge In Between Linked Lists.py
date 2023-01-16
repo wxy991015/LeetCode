2f97a8fbdb58e9141a8874745ed8d8767b90d6d1
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-15 20:21:43 -0600

    update LeetCode Problem 1669

--- Medium/Merge In Between Linked Lists.py
+++ Medium/Merge In Between Linked Lists.py
@@ -4,6 +4,8 @@ class ListNode:
         self.next = next
 
 # version 1
+
+
 def mergeInBetween(list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
     start = list1
     count = 1
@@ -11,13 +13,13 @@ def mergeInBetween(list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode
     while count != a:
         count += 1
         start = start.next
-    
+
     # get the end of removing
     end = start.next
     while count != b:
         count += 1
         end = end.next
-    
+
     # insert list2 between start and end
     while list2:
         start.next = list2
@@ -28,24 +30,24 @@ def mergeInBetween(list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode
 
 # version 2
 def mergeInBetween1(list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-    curr=list1
-    t=0
-    var=None
-    while curr!=None:
-        t+=1
-        if t==b+1:
+    curr = list1
+    t = 0
+    var = None
+    while curr != None:
+        t += 1
+        if t == b+1:
             break
-        if a==t:
-            pp=curr.next
-            var=curr
-            var.next=list2
+        if a == t:
+            pp = curr.next
+            var = curr
+            var.next = list2
             # curr.next=None
-            curr=pp
-        curr=curr.next
-    temp=list1
-    while temp!=None:
-        if temp.next==None:
+            curr = pp
+        curr = curr.next
+    temp = list1
+    while temp != None:
+        if temp.next == None:
             break
-        temp=temp.next
-    temp.next=curr
+        temp = temp.next
+    temp.next = curr
     return list1
