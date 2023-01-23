1ee973104826ea351593413b6c5e3cb57232c785
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-23 01:06:14 -0600

    update solution of LeetCode Problem 2545

--- Medium/Sort the Students by Their Kth Score.py
+++ Medium/Sort the Students by Their Kth Score.py
@@ -2,8 +2,7 @@ from typing import List
 from operator import itemgetter
 
 def sortTheStudents(score: List[List[int]], k: int) -> List[List[int]]:
-    score.sort(key=itemgetter(k), reverse=True)
-    return score
+    return sorted(score, key=itemgetter(k), reverse=True)
 
 score = [[10,6,9,1],[7,5,11,2],[4,8,3,15]]
 k = 2
