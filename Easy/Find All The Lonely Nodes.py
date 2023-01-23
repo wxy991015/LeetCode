c42d63389cdb919a8e78ac50e8b311956b8bfa89
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-22 19:39:16 -0600

    iterative solution of 1469

--- Easy/Find All The Lonely Nodes.py
+++ Easy/Find All The Lonely Nodes.py
@@ -6,6 +6,7 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# version 1 - recursion
 def getLonelyNodes(self, root: Optional[TreeNode]) -> List[int]:
     result = []
     def solver(root: Optional[TreeNode], result: List[int]) -> None:
@@ -19,3 +20,42 @@ def getLonelyNodes(self, root: Optional[TreeNode]) -> List[int]:
         solver(root.right, result)
     solver(root, result)
     return result
+
+# version 2 - iteration
+def getLonelyNodes(self, root: Optional[TreeNode]) -> List[int]:
+    stk = [root]
+    result = []
+    while stk:
+        node = stk.pop()
+        if node.left:
+            stk.append(node.left)
+            if not node.right:
+                result.append(node.left.val)
+        if node.right:
+            stk.append(node.right)
+            if not node.left:
+                result.append(node.right.val)
+    return result
+
+"""
+def getLonelyNodes(self, root: TreeNode) -> List[int]:
+        stk = [root]
+        res = []
+        
+        while stk:
+            node = stk.pop()
+            
+            if node.left:
+                stk.append(node.left)
+                
+                if not node.right:
+                    res.append(node.left.val)
+                    
+            if node.right:
+                stk.append(node.right)
+                
+                if not node.left:
+                    res.append(node.right.val)
+        
+        return res
+"""
