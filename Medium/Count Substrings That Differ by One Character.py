815518db6c79adb8880d5116c44cb296a59977e1
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-23 00:28:02 -0600

    update LeetCode Problem of 1638

--- Medium/Count Substrings That Differ by One Character.py
+++ Medium/Count Substrings That Differ by One Character.py
@@ -1,5 +1,14 @@
+# version 1 - time limit exceeded
 def differenceByOne(s: str, t: str) -> bool:
-    
+    difference = 0
+    if len(s) != len(t):
+        return False
+    for i in range(len(s)):
+        if s[i] != t[i]:
+            difference += 1
+            if difference > 1:
+                return False
+    return difference == 1
 
 def countSubstrings(s: str, t: str) -> int:
     result = 0
@@ -7,9 +16,12 @@ def countSubstrings(s: str, t: str) -> int:
         for j in range(i+1, len(s)+1):
             s_substring = s[i:j]
             for k in range(len(t)):
-                for w in range(i+1, len(t)+1):
+                for w in range(k+1, len(t)+1):
                     t_substring = t[k:w]
-                    if len(s_substring) == len(t_substring) and s_substring != t_substring:
-                        
-                        
+                    if differenceByOne(s_substring, t_substring):
+                        result += 1
     return result
+
+s = "ab"
+t = "bb"
+print(f"Output: {countSubstrings(s, t)}")
