4b3c240cbf2dc7d3263b230dad9c51afe8e5e1de
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-25 01:53:23 -0600

    update solution of LeetCode Problem 2044

--- Medium/Count Number of Maximum Bitwise-OR Subsets.py
+++ Medium/Count Number of Maximum Bitwise-OR Subsets.py
@@ -1,6 +1,22 @@
 from typing import List
 
 def countMaxOrSubsets(nums: List[int]) -> int:
-    number_of_subsets = 0
-    subsets = []
-    return number_of_subsets
+    temp, result = [], []
+    def solver(start: int, temp: List[int], output: List[List[int]]) -> None:
+        if len(temp) != 0:
+            result.append(temp)
+        for i in range(start, len(nums)):
+            solver(i+1, temp + [nums[i]], output)
+    solver(0, temp, result)
+    maximum_bitwise = bitwise_result = 0
+    for i in range(len(result)):
+        subset = result[i]
+        temp_max = subset[0]
+        for j in range(1, len(subset)):
+            temp_max |= subset[j]
+        if temp_max > maximum_bitwise:
+            bitwise_result = 1
+            maximum_bitwise = temp_max
+        elif temp_max == maximum_bitwise:
+            bitwise_result += 1
+    return bitwise_result
