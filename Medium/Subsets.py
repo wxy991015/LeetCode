44e8c41705d343edbc8dd15d735f499f389e3dcf
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-25 21:44:44 -0600

    update solution of LeetCode Problem 87

--- Medium/Subsets.py
+++ Medium/Subsets.py
@@ -1,7 +1,10 @@
 from typing import List
 
+# version 1 - use list comprehension
 def subsets(nums: List[int]) -> List[List[int]]:
     result = [[]]
     for i in nums:
         result += [lst + [i] for lst in result]
     return result
+
+# version 2 - use backtrack
