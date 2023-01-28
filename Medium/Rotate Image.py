389c0004209bd1a8731a0d7babb2d382e95aae01
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-28 16:31:38 -0600

    update solution of LeetCode Problem 48

--- Medium/Rotate Image.py
+++ Medium/Rotate Image.py
@@ -4,4 +4,20 @@ def rotate(matrix: List[List[int]]) -> None:
     """
     Do not return anything, modify matrix in-place instead.
     """
+    n = len(matrix)
+    def solver(matrix: List[List[int]], offset: int, n: int) -> None:
+        if offset >= n:
+            return
+        length = len(matrix) - 1
+        for i in range(offset, n):
+            t = matrix[offset][i]
+            matrix[offset][i] = matrix[length-i][offset]
+            matrix[length-i][offset] = matrix[length-offset][length-i]
+            matrix[length-offset][length-i] = matrix[i][length-offset]
+            matrix[i][length-offset] = t
+        solver(matrix, offset+1, n-1)
+    solver(matrix, 0, n-1)
     
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+rotate(matrix)
+print(matrix)
