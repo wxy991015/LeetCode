78f7ec3f7b47f0ac819f6e03cebc9ae76646106c
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-01-30 02:56:03 -0600

    create Python file of LeetCode Problem 938

---
+++ Easy/Range Sum of BST.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+     
+# version 1: recursion   
+def rangeSumBST1(root: Optional[TreeNode], low: int, high: int) -> int:
+    pass
+
+# version 2: iteration
+def rangeSumBST2(root: Optional[TreeNode], low: int, high: int) -> int:
+    pass
