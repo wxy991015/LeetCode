34bfeec205fab925e213b8312d225f6737ab6b02
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-01 00:59:55 -0600

    recursive solution of Problem 1315

--- Medium/Sum of Nodes with Even-Valued Grandparent.py
+++ Medium/Sum of Nodes with Even-Valued Grandparent.py
@@ -4,5 +4,20 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def sumEvenGrandparent(self, root: TreeNode) -> int:
-    pass
+# version 1 - recursion
+def sumEvenGrandparent(root: TreeNode) -> int:
+    sum_of_values = 0
+    if not root:
+        return 0
+    if root.val % 2 == 0:
+        if root.left and root.left.left:
+            sum_of_values += root.left.left.val
+        if root.left and root.left.right:
+            sum_of_values += root.left.right.val
+        if root.right and root.right.left:
+            sum_of_values += root.right.left.val
+        if root.right and root.right.right:
+            sum_of_values += root.right.right.val
+    sum_of_values += sumEvenGrandparent(root.left)
+    sum_of_values += sumEvenGrandparent(root.right)
+    return sum_of_values
