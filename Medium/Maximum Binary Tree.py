4a383ae36d04b25e3710994c42195fb37bcbe28a
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-01 01:19:32 -0600

    new Problem 654

---
+++ Medium/Maximum Binary Tree.py
@@ -0,0 +1,10 @@
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def constructMaximumBinaryTree(nums: List[int]) -> Optional[TreeNode]:
+    pass
