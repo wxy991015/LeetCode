9ae93a4ed3c548fc12aa56a077bfb04f0793319b
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-02 19:51:57 -0600

    new Problem 617

---
+++ Easy/Merge Two Binary Trees.py
@@ -0,0 +1,10 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+def mergeTrees(root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+    pass
