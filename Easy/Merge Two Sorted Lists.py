0795d8e6e27cba68228b25a162e58d753120116e
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-03 14:08:55 -0600

    updated solution of Problem 21

--- Easy/Merge Two Sorted Lists.py
+++ Easy/Merge Two Sorted Lists.py
@@ -5,7 +5,8 @@ class ListNode:
         self.val = val
         self.next = next
 
-def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+# version 1 - original version of merge
+def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
     p = ListNode()
     merged_result = p
     while list1 and list2:
@@ -26,5 +27,24 @@ def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optio
             p.next = ListNode(list2.val)
             list2 = list2.next
             p = p.next
+    merged_result = merged_result.next
+    return merged_result
+
+# version 2 - updated version of merge
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    p = ListNode()
+    merged_result = p
+    while list1 and list2:
+        if list1.val <= list2.val:
+            p.next = list1
+            list1 = list1.next
+        else:
+            p.next = list2
+            list2 = list2.next
+        p = p.next
+    if list1:
+        p.next = list1
+    if list2:
+        p.next = list2
     merged_result = merged_result.next
     return merged_result
