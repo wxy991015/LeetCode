edbb0dfaa0df8ce14ed74efb896667ce7c372594
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-03 01:45:43 -0600

    create new Problem 1382

--- Medium/Balance a Binary Search Tree.py
+++ Medium/Balance a Binary Search Tree.py
@@ -1,24 +1,13 @@
+from typing import List
+import bisect
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-        
+
+# version 1 - brutal force
+# use post order traversal for binary search tree
 def balanceBST(root: TreeNode) -> TreeNode:
-    if not root.left and not root.right:
-        return root
-    result = TreeNode(root.val)
-    result_left, result_right = result.left, result.right
-    def helper(root: TreeNode) -> None:
-        nonlocal result, result_left, result_right
-        if not root: return
-        if not result_left:
-            result_left = TreeNode(root.val)
-            result_left = result_left.left
-        if not result_right:
-            result_right = TreeNode(root.val)
-            result_right = result_right.right
-        helper(root.left)
-        helper(root.right)
-    return result
-        
+    pass
