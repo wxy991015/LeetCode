d1ccff39412001ed018ac5b4d1caa6161f2f3369
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-03 01:51:27 -0600

    recursive solution of Problem 1382

--- Medium/Balance a Binary Search Tree.py
+++ Medium/Balance a Binary Search Tree.py
@@ -8,6 +8,21 @@ class TreeNode:
         self.right = right
 
 # version 1 - brutal force
-# use post order traversal for binary search tree
+# use post order traversal to collect binary search tree
 def balanceBST(root: TreeNode) -> TreeNode:
-    pass
+    nodes = []
+    def post_order(root: TreeNode) -> None:
+        if root:
+            post_order(root.left)
+            nodes.append(root.val)
+            post_order(root.right)
+    post_order(root)
+    def buid_tree(nodes: List[int]):
+        if not nodes:
+            return
+        mid = len(nodes) // 2
+        result = TreeNode(nodes[mid])
+        result.left = buid_tree(nodes[0:mid])
+        result.right = buid_tree(nodes[mid+1:])
+        return result
+    return buid_tree(nodes)
