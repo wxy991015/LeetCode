4fe789337accf56ef383409a06b17c91b121d4c6
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-03 01:19:23 -0600

    brute force solution of Problem 897

--- Easy/Increasing Order Search Tree.py
+++ Easy/Increasing Order Search Tree.py
@@ -1,15 +1,29 @@
+from typing import List
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+# version 1 - brute force
 def increasingBST(root: TreeNode) -> TreeNode:
-    result_root = None
-    def helper(root: TreeNode):
-        nonlocal result_root
-        if root.left:
-            helper(root.left)
-        result_root = TreeNode(root.val)
-        if root.right:
-            pass
+    if not root:
+        return None
+    nodes = []
+    def nodes_collections(root: TreeNode, nodes: List[int]) -> None:
+        if not root:
+            return
+        nodes.append(root.val)
+        nodes_collections(root.left, nodes)
+        nodes_collections(root.right, nodes)
+    nodes_collections(root, nodes)
+    nodes = sorted(nodes)
+    p = TreeNode()
+    result = p
+    for i in nodes:
+        p.right = TreeNode(i)
+        p = p.right
+    return result.right
+
+# version 2
