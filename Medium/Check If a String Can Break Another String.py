0787519e9d692bdfd0747b9654cf0c917e852f74
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-04 23:49:44 -0600

    create new Problem 1433

--- Medium/Check If a String Can Break Another String.py
+++ Medium/Check If a String Can Break Another String.py
@@ -1,5 +1,8 @@
 from typing import List
 
 def checkIfCanBreak(self, s1: str, s2: str) -> bool:
-    def permutation(temp: str, collections: List[int], current: int, steps: int) -> None:
-        pass
+    def permutation(temp: str, s: str, collections: List[int], current: int, steps: int) -> None:
+        if current < steps:
+            for k in range(len(s)):
+                current_str = s[k]
+                
