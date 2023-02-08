50e00b0855515e4e90ff0d19ea6376238da93256
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-07 23:41:21 -0600

    modify Problem 4

--- Hard/Median of Two Sorted Arrays.py
+++ Hard/Median of Two Sorted Arrays.py
@@ -1,24 +1,23 @@
 from typing import List
 
-def mergeSort(nums1: List[int], nums2: List[int]):
-    result = []
-    i = j = 0
-    while i < len(nums1) and j < len(nums2):
-        if nums1[i] <= nums2[j]:
+def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+    def mergeSort(nums1: List[int], nums2: List[int]):
+        result = []
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] <= nums2[j]:
+                result.append(nums1[i])
+                i += 1
+            else:
+                result.append(nums2[j])
+                j += 1
+        while i < len(nums1):
             result.append(nums1[i])
             i += 1
-        else:
+        while j < len(nums2):
             result.append(nums2[j])
             j += 1
-    while i < len(nums1):
-        result.append(nums1[i])
-        i += 1
-    while j < len(nums2):
-        result.append(nums2[j])
-        j += 1
-    return result
-
-def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+        return result
     merged_nums = mergeSort(nums1, nums2)
     print(merged_nums)
     if len(merged_nums) == 0:
