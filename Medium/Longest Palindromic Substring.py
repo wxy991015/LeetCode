c963df160d07c0ab9381e36d8571e9aaeb2253b0
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-08 00:51:48 -0600

    solution of Problem 5

--- Medium/Longest Palindromic Substring.py
+++ Medium/Longest Palindromic Substring.py
@@ -1,6 +1,25 @@
 def longestPalindrome(s: str) -> str:
-    longest_palindrome = ""
-    stack = []
+    res = ""
+    resLen = 0
     for i in range(len(s)):
-        pass
-    return longest_palindrome
+        # odd length
+        l, r = i, i
+        while l >= 0 and r <len(s) and s[l] == s[r]:
+            if (r - l + 1) > resLen:
+                res = s[l:r+1]
+                resLen = r - l + 1
+            l -= 1
+            r += 1
+            
+        # even length
+        l, r = i, i + 1
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r - l + 1) > resLen:
+                res = s[l:r+1]
+                resLen = r - l + 1
+            l -= 1
+            r += 1
+    return res
+
+s = "cbbd"
+print(f"Output: {longestPalindrome(s)}")
