b81e5a6b14853f778b80332e85bec3f4817f45ff
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-09 01:36:03 -0600

    solution of Problem 8

--- Medium/String to Integer (atoi).py
+++ Medium/String to Integer (atoi).py
@@ -1,11 +1,29 @@
 def myAtoi(s: str) -> int:
+    s = s.strip()
+    if not s:
+        return 0
     num_result = 0
-    word_phrase = s.split(" ")
-    for i in range(len(word_phrase)):
-        word = word_phrase[i]
-        word_copy = word.strip("+-")
-        if word_copy.isdigit():
-            num_result = int(word_copy)
-            if word[0] == "-":
-                num_result = -num_result
+    digits = ""
+    sign = 1
+    i = 0
+    if s[0] == "-":
+        sign = -1
+        i += 1
+    elif s[0] == "+":
+        i += 1
+    while i < len(s):
+        if s[i].isdigit():
+            digits += s[i]
+        else:
+            break
+        i += 1
+    if len(digits) > 0:
+        num_result = int(digits) * sign
+    if num_result < -2 ** 31:
+        num_result = -2 ** 31
+    elif num_result > 2 ** 31 - 1:
+        num_result = 2 ** 31 - 1
     return num_result
+
+s = "4193 with words"
+print(f"Output: {myAtoi(s)}")
