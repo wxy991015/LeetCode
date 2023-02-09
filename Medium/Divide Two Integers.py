5ec533798606d5563bf8b0b031f11992cb48d4af
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-09 17:06:58 -0600

    continuous subtraction solution of Problem 29

--- Medium/Divide Two Integers.py
+++ Medium/Divide Two Integers.py
@@ -1,3 +1,4 @@
+# use whole number divisor
 def divide(dividend: int, divisor: int) -> int:
     if dividend == -2147483648 and divisor == -1: 
         return 2147483647
@@ -5,6 +6,26 @@ def divide(dividend: int, divisor: int) -> int:
     if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0):
         integer_part *= -1
     return integer_part
+
+# use continuous subtraction
+def divide(dividend: int, divisor: int) -> int:
+    negatives = 0
+    if dividend < 0:
+        negatives += 1
+        dividend = -dividend
+    
+    if divisor < 0:
+        negatives += 1
+        divisor = -divisor
+    
+    substrations = 0
+    while (dividend - divisor >= 0):
+        substrations += 1
+        dividend -= divisor
+    
+    if negatives == 1:
+        substrations = -substrations
+    return substrations
 
 dividend = 7
 divisor = -3
