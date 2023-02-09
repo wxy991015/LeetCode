d9e21df05788b3b3217816d70c1b2216647333bc
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-09 01:15:51 -0600

    solution of Problem 15

--- Medium/3Sum.py
+++ Medium/3Sum.py
@@ -1,16 +1,23 @@
 from typing import List
 
 def threeSum(nums: List[int]) -> List[List[int]]:
-    if len(nums) < 3:
-        return []
     result = []
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            for k in range(j+1, len(nums)):
-                temp = [nums[i], nums[j], nums[k]]
-                temp = sorted(temp)
-                if sum(temp) == 0 and temp not in result:
-                    result.append(temp)
+    nums.sort()
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i-1]:
+            continue
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            temp_sum = a + nums[l] + nums[r]
+            if temp_sum > 0:
+                r -= 1
+            elif temp_sum < 0:
+                l += 1
+            else:
+                result.append([a, nums[l], nums[r]])
+                l += 1
+                while nums[l] == nums[l-1] and l < r:
+                    l += 1
     return result
 
 nums = [-1,0,1,2,-1,-4]
