4dca6f1e7d811e005d6039ec4817d9e06445e5d9
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-10 00:20:55 -0600

    solution of Problem 6

--- Medium/Zigzag Conversion.py
+++ Medium/Zigzag Conversion.py
@@ -1,31 +1,13 @@
 def convert(s: str, numRows: int) -> str:
-    numColumns = len(s) // numRows
-    numColumns = numColumns + (numColumns - 1) * (numRows - 2)
-    collections = []
-    for i in range(numRows):
-        collections.append([""] * numColumns)
-    print(collections)
-    xIndex, yIndex = 0, 0
-    i = 0
-    while i < len(s):
-        if xIndex < numRows:
-            collections[xIndex][yIndex] = s[i]
-            xIndex += 1
-            i += 1
-        else:
-            xIndex -= 2
-            yIndex += 1
-            while xIndex > 0:
-                print(xIndex, yIndex)
-                collections[xIndex][yIndex] = s[i]
-                yIndex += 1
-                i += 1
-                xIndex -= 1
-        print(collections)
-    result = ""
-    for i in range(len(collections)):
-        result += "".join(collections[i])
-    return result
+    if numRows == 1: return s
+    res = ""
+    for r in range(numRows):
+        increment = 2 * (numRows - 1)
+        for i in range(r, len(s), increment):
+            res += s[i]
+            if r > 0 and r < numRows - 1 and i + increment - 2 * r < len(s):
+                res += s[i + increment - 2 * r]
+    return res
 
 s = "PAYPALISHIRING"
 numRows = 3
