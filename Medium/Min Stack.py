e6b0b7efa537c97c6c2a2d0a3d11b3776ccb1a51
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-10 21:57:32 -0600

    solution of Problem 155

---
+++ Medium/Min Stack.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+# version 1 - use list stack
+class MinStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop(-1)
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+# version 2 - use deque
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque()
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
