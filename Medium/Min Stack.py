c417f8434ae82079137415355123d67f27b921e0
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-10 23:16:53 -0600

    delete one solution of Problem 49

--- Medium/Min Stack.py
+++ Medium/Min Stack.py
@@ -16,21 +16,3 @@ class MinStack:
 
     def getMin(self) -> int:
         return min(self.stack)
-
-# version 2 - use deque
-class MinStack:
-
-    def __init__(self):
-        self.stack = deque()
-
-    def push(self, val: int) -> None:
-        self.stack.append(val)
-
-    def pop(self) -> None:
-        self.stack.pop()
-
-    def top(self) -> int:
-        return self.stack[-1]
-
-    def getMin(self) -> int:
-        return min(self.stack)
