722c747366c65248ef901c29a6b2f5b806b4ab1c
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-12 16:31:54 -0600

    two solutions of Problem 151

---
+++ Medium/Reverse Words in a String.py
@@ -0,0 +1,36 @@
+# version 1 - loop through sentence
+def reverseWords(s: str) -> str:
+    s = s.strip()
+    words = []
+    word = ""
+    for i in range(len(s)):
+        if s[i] == " ":
+            if len(word) != 0:
+                words.append(word)
+                word = ""
+            continue
+        word += s[i]
+    words.append(word)
+    reversed_res = ""
+    i = len(words) - 1
+    while i >= 0:
+        reversed_res += words[i] + " "
+        i -= 1
+    reversed_res = reversed_res.strip()
+    return reversed_res
+
+# version 2 - use split method
+def reverseWords1(s: str) -> str:
+    s = s.strip()
+    words = s.split(" ")
+    print(words)
+    reversed_res = ""
+    for i in range(len(words)-1, -1, -1):
+        if words[i] == "":
+            continue
+        reversed_res += words[i] + " "
+    reversed_res = reversed_res.strip()
+    return reversed_res
+
+s = "a good   example"
+print(f"Output: {reverseWords1(s)}")
