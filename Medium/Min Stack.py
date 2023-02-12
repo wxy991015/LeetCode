6ce16f1353ec20876e98e1951cde1edc3f703e09
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-12 16:19:00 -0600

    update solution of Problem 155

--- Medium/Min Stack.py
+++ Medium/Min Stack.py
@@ -16,3 +16,25 @@ class MinStack:
 
     def getMin(self) -> int:
         return min(self.stack)
+
+# version 2 - update stack method
+class MinStack:
+    def __init__(self):
+        self.stack = []
+
+    # push and note the current min value
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+            return
+        current_min = self.stack[-1][1]
+        self.stack.append((val, min(current_min, val)))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
