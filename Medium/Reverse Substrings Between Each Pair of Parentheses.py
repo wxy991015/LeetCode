63ca7de092ed39578f97536404e010d90c38ca89
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-15 00:30:46 -0600

    solution of Problem 1190

--- Medium/Reverse Substrings Between Each Pair of Parentheses.py
+++ Medium/Reverse Substrings Between Each Pair of Parentheses.py
@@ -1,15 +1,17 @@
 def reverseParentheses(s: str) -> str:
+    s_list = list(s)
     res = ""
     open = []
-    for i in range(len(s)):
-        if s[i] == "(":
+    for i in range(len(s_list)):
+        if s_list[i] == "(":
             open.append(i)
-        elif s[i] == ")":
-            sub = s[open[-1]+1:i]
-            sub = sub[::-1]
-            open.pop()
-            res += sub
+        elif s_list[i] == ")":
+            start = open.pop()
+            s_list[start:i+1] = s_list[start:i+1][::-1]
+    for i in range(len(s_list)):
+        if s_list[i] != "(" and s_list[i] != ")":
+            res += s_list[i]
     return res
 
-s = "(u(love)i)"
+s = "(ed(et(oc))el)"
 print(f"Output: {reverseParentheses(s)}")
