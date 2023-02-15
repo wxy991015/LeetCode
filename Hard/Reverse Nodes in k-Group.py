8fd0820b19af7987c7e29f16d4ff574d4c9e50fa
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-15 00:12:15 -0600

    brute force solution of Problem 25

--- Hard/Reverse Nodes in k-Group.py
+++ Hard/Reverse Nodes in k-Group.py
@@ -10,5 +10,30 @@ class ListNode:
         self.val = val
         self.next = next
 
+# version 1 - brute force
 def reverseKGroup(head: Optional[ListNode], k: int) -> Optional[ListNode]:
-    return head
+    nodes = []
+    while head:
+        nodes.append(head.val)
+        head = head.next
+    i = 0
+    while i + k <= len(nodes):
+        nodes[i:i+k] = nodes[i:i+k][::-1]
+        i += k
+    p = ListNode()
+    res = p
+    for i in nodes:
+        p.next = ListNode(i)
+        p = p.next
+    return res.next
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+k = 3
+res = reverseKGroup(head, k)
+while res:
+    print(res.val)
+    res = res.next
