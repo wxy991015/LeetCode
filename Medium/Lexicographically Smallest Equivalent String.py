c212c0b6a390afad61033c985e20fef01875a0fa
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-16 17:53:49 -0600

    update Problem 1061

--- Medium/Lexicographically Smallest Equivalent String.py
+++ Medium/Lexicographically Smallest Equivalent String.py
@@ -1,13 +1,33 @@
 def smallestEquivalentString(s1: str, s2: str, baseStr: str) -> str:
     smallest_equivalent_string = ""
-    equivalent_record = dict()
+    rules = []
     for i in range(len(s1)):
-        if s1[i] in equivalent_record:
-            equivalent_record[s1[i]].add(s1[i])
-            equivalent_record[s2[i]].add(s2[i])
-        else:
-            equivalent_record[s1[i]] = set()
+        current_s1 = s1[i]
+        current_s2 = s2[i]
+        j = 0
+        while j < len(rules):
+            if current_s1 in rules[j] or current_s2 in rules[j]:
+                rules[j].add(current_s1)
+                rules[j].add(current_s2)
+                break
+            j += 1
+        if j == len(rules):
+            sub_rules = {current_s1, current_s2}
+            rules.append(sub_rules)
+    print(rules)
     for i in range(len(baseStr)):
-        current_str = baseStr[i]
-        # continue
+        current = baseStr[i]
+        j = 0
+        while j < len(rules):
+            if current in rules[j]:
+                smallest_equivalent_string += sorted(list(rules[j]))[0]
+                break
+            j += 1
+        if j == len(rules):
+            smallest_equivalent_string += current
     return smallest_equivalent_string
+
+s1 = "leetcode"
+s2 = "programs"
+baseStr = "sourcecode"
+print(f"Output: {smallestEquivalentString(s1, s2, baseStr)}")
