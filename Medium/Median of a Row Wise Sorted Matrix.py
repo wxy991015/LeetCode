03cbfc1c8ed1f0010393eb71ad387072b9a9e901
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-21 13:06:45 -0600

    update Problem 2387

--- Medium/Median of a Row Wise Sorted Matrix.py
+++ Medium/Median of a Row Wise Sorted Matrix.py
@@ -1,11 +1,21 @@
 from typing import List
 
+# version 1 - brute force
 def matrixMedian(grid: List[List[int]]) -> int:
     nums = []
     for i in grid:
         nums.extend(i)
     nums.sort()
     return nums[len(nums)//2]
+
+# version 2 - binary search
+def matrixMedian(grid: List[List[int]]) -> int:
+    m, n = len(grid), len(grid[0])
+    itemsNum = m * n
+    l = min(row[0] for row in grid)
+    r = max(row[-1] for row in grid)
+    print(l, r)
+    
 
 grid = [[1,1,3,3,4]]
 print(f"Output: {matrixMedian(grid)}")
