d63bd255b9520c90895dce55dba07c82af08291b
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-02-27 16:09:21 -0600

    create Problem of 239

--- Hard/Sliding Window Maximum.py
+++ Hard/Sliding Window Maximum.py
@@ -1,4 +1,19 @@
 from typing import List
 
 def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
-    pass
+    monoStack = [nums[0]]
+    res = []
+    for i in range(len(nums)):
+        if nums[i] <= monoStack[-1]:
+            monoStack.append(nums[i])
+        else:
+            while len(monoStack) > 0 and nums[i] > monoStack[-1]:
+                monoStack.pop()
+            monoStack.append(nums[i])
+        if i >= k - 1:
+            res.append(monoStack[0])
+    return res
+
+nums = [1]
+k = 1
+print(f"Output: {maxSlidingWindow(nums, k)}")
