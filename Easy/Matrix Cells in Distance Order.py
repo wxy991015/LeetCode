2358e9005b87e1ac275cddb92a81bfd6d17bc5c8
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-01 23:51:55 -0600

    create Problem 1030

---
+++ Easy/Matrix Cells in Distance Order.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def allCellsDistOrder(rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
+    res = [[rCenter, cCenter]]
+    leftRow, rightRow = rCenter, rCenter
+    leftCol, rightCol = cCenter, cCenter
+    
+    return res
+
+"""
+...
+...
+...
+...
+"""
