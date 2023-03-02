807867a7ec71685cf9cb7bb83439d9265718bce7
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-01 22:38:47 -0600

    second solution of Problem 2574

--- Easy/Left and Right Sum Differences.py
+++ Easy/Left and Right Sum Differences.py
@@ -15,5 +15,22 @@ def leftRigthDifference(nums: List[int]) -> List[int]:
         res.append(abs(leftSum[i]-rightSum[len(nums)-1-i]))
     return res
 
+# version 2
+def leftRigthDifference1(nums: List[int]) -> List[int]:
+    size = len(nums)
+    leftSum = [0] * size
+    rightSum = [0] * size
+    res = [0] * size
+    currLeftSum, currRightSum = 0, 0
+    for i in range(size):
+        leftSum[i] = currLeftSum
+        currLeftSum += nums[i]
+    for i in range(size-1, -1, -1):
+        rightSum[size-i-1] = currRightSum
+        currRightSum += nums[i]
+    for i in range(size):
+        res[i] = abs(leftSum[i]-rightSum[size-i-1])
+    return res
+
 nums = [10,4,8,3]
-print(f"Output: {leftRigthDifference(nums)}")
+print(f"Output: {leftRigthDifference1(nums)}")
