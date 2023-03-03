51587718ce512f71b578fcf265b36472a7ede4e9
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-02 23:47:05 -0600

    solution of Problem 2451

--- Easy/Two Sum Less Than K.py
+++ Easy/Two Sum Less Than K.py
@@ -2,20 +2,20 @@ from typing import List
 
 def twoSumLessThanK(nums: List[int], k: int) -> int:
     nums.sort()
-    if len(nums) == 1 or nums[0] > k:
+    if len(nums) == 1 or nums[0] + nums[1] >= k:
         return -1
     print(nums)
     res = 0
-    low, high = 0, len(nums) - 1
-    while low <= high:
-        mid = (low + high) // 2
-        if nums[mid] >= k / 2:
-            high = mid
-        else:
-            if nums[mid] + nums[mid-1]:
-                pass
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            val = nums[i] + nums[j]
+            if val >= k:
+                break
+            else:
+                res = max(res, val)
+    return res
         
 
-nums = [34,23,1,24,75,33,54,8]
-k = 60 
+nums = [10,20,30]
+k = 15
 print(f"Output: {twoSumLessThanK(nums, k)}")
