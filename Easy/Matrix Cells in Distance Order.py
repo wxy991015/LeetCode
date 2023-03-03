c8e10d3f6c9ad7eb6d0b3ab3a0139621edab56a7
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-02 23:40:11 -0600

    solution of Problem 1030

--- Easy/Matrix Cells in Distance Order.py
+++ Easy/Matrix Cells in Distance Order.py
@@ -1,11 +1,29 @@
 from typing import List
 
 def allCellsDistOrder(rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
-    res = [[rCenter, cCenter]]
-    leftRow, rightRow = rCenter, rCenter
-    leftCol, rightCol = cCenter, cCenter
-    
+    def cellDist(cell: List[int], rCenter: int, cCenter: int) -> int:
+        return abs(cell[0]-rCenter) + abs(cell[1]-cCenter)
+    if rows == 1 and cols == 1:
+        return [[rCenter, cCenter]]
+    cellDistances = dict()
+    for i in range(rows):
+        for j in range(cols):
+            cell = [i, j]
+            cellDistance = cellDist(cell, rCenter, cCenter)
+            if cellDistance in cellDistances:
+                cellDistances[cellDistance].append(cell)
+            else:
+                cellDistances[cellDistance] = [cell]
+    res = []
+    for key in sorted(cellDistances):
+        res.extend(cellDistances[key])
     return res
+
+rows = 2
+cols = 3
+rCenter = 1
+cCenter = 2
+print(f"Output: {allCellsDistOrder(rows, cols, rCenter, cCenter)}")
 
 """
 ...
