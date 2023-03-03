c42d1a87ecac09dba9d7605f3807da9bac27e90f
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-02 23:39:54 -0600

    solution of Problem 2544

--- Easy/Alternating Digit Sum.py
+++ Easy/Alternating Digit Sum.py
@@ -1,33 +1,22 @@
 def alternateDigitSum(n: int) -> int:
-    def splitDigits(n: int):
+    def getDigits(n: int):
         digits = []
         while n != 0:
             digits.append(n%10)
             n //= 10
         digits.reverse()
         return digits
-    digits = splitDigits(n)
-    maxIndex = digits.index(max(digits))
-    res = max(digits)
-    left, right = maxIndex, maxIndex
-    leftFlag, rightFlag = True, True
-    while left > 0:
-        if leftFlag:
-            res -= digits[left-1]
-            leftFlag = False
+    res = 0
+    digits = getDigits(n)
+    flag = True
+    for i in range(len(digits)):
+        if flag:
+            res += digits[i]
+            flag = False
         else:
-            res += digits[left-1]
-            leftFlag = True
-        left -= 1
-    while right < len(digits)-1:
-        if rightFlag:
-            res -= digits[right+1]
-            rightFlag = False
-        else:
-            res += digits[right+1]
-            rightFlag = True
-        right += 1
+            res -= digits[i]
+            flag = True
     return res
 
-n = 886996
+n = 25
 print(f"Output: {alternateDigitSum(n)}")
