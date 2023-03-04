db72f51af7a43f09eea3487f4bbae3a8431f08e2
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-04 00:38:04 -0600

    dynamic Programming Solution of Problem 560

--- Medium/Subarray Sum Equals K.py
+++ Medium/Subarray Sum Equals K.py
@@ -1,4 +1,12 @@
 from typing import List
 
 def subarraySum(nums: List[int], k: int) -> int:
-    
+    res = 0
+    currentSum = 0
+    prefixSums = {0:1}
+    for n in nums:
+        currentSum += n
+        diff = currentSum - k
+        res += prefixSums.get(diff, 0)
+        prefixSums[currentSum] = 1 + prefixSums.get(currentSum, 0)
+    return res
