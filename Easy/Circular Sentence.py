a7f09cddaef5a3aad4a6f5e8392e8b953b46a145
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-06 13:06:19 -0600

    solution of Problem 2490

--- Easy/Circular Sentence.py
+++ Easy/Circular Sentence.py
@@ -1,2 +1,14 @@
 def isCircularSentence(sentence: str) -> bool:
-    pass
+    words = sentence.split()
+    for i in range(len(words)):
+        word = words[i]
+        if i == len(words) - 1:
+            if word[-1] != words[0][0]:
+                return False
+        else:
+            if word[-1] != words[i+1][0]:
+                return False
+    return True
+
+sentence = "Leetcode is cool"
+print(f"Output: {isCircularSentence(sentence)}")
