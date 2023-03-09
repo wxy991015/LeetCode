1171c1e58d85abf2d00cfb14311b36c0188fcc4e
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-08 20:26:26 -0600

    solution of Problem 2437

---
+++ Easy/Number of Valid Clock Times.py
@@ -0,0 +1,23 @@
+def countTime(time: str) -> int:
+    res = 1
+    if time[0] == "?":
+        if time[1] == "?" or "0" <= time[1] <= "3":
+            res *= 3
+        else:
+            res *= 2
+    if time[1] == "?":
+        if time[0] == "0" or time[0] == "1":
+            res *= 10
+        else:
+            if time[0] == "?":
+                res = 24
+            else:
+                res *= 4
+    if time[3] == "?":
+        res *= 6
+    if time[4] == "?":
+        res *= 10
+    return res
+
+time = "??:??"
+print(f"Output: {countTime(time)}")
