50edcb2eccca97ea519359769a9119bf1bb70e22
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-09 23:54:21 -0600

    solution of Problem 2451

--- Easy/Odd String Difference.py
+++ Easy/Odd String Difference.py
@@ -1,18 +1,22 @@
 from typing import List
 
 def oddString(words: List[str]) -> str:
-    differenceArrays = []
+    differenceArrays = dict()
     for i in range(len(words)):
         word = words[i]
         difference = []
         for j in range(1, len(word)):
             difference.append(ord(word[j])-ord(word[j-1]))
-        if not difference in differenceArrays:
-            if len(differenceArrays) <= 1:
-                differenceArrays.append(difference)
-            else:
-                if difference == differenceArrays[0]:
-                    return di
+        difference = tuple(difference)
+        if difference in differenceArrays:
+            differenceArrays[difference].append(word)
+        else:
+            differenceArrays[difference] = [word]
+    print(differenceArrays.items())
+    for key in differenceArrays:
+        if len(differenceArrays[key]) == 1:
+            return differenceArrays[key][0]
+    return ""
            
 
 words = ["aaa","bob","ccc","ddd"]
