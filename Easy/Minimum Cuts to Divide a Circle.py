105f2c9095644654c73f007879c45e76c37056b4
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-09 21:04:28 -0600

    solution of Problem 2481

---
+++ Easy/Minimum Cuts to Divide a Circle.py
@@ -0,0 +1,13 @@
+def numberOfCuts(n: int) -> int:
+    if n == 1:
+        return 0
+    if n % 2 == 0:
+        return n // 2
+    return n
+
+def numberOfCuts(self, n: int) -> int:
+    if n==1: return 0
+    return n//2 if n%2==0 else n
+
+n = 4
+print(f"Output: {numberOfCuts(n)}")
