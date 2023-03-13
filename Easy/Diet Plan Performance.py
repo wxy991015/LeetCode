872c5be2870f9d6db9488cd8d8cd900b1add979e
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-13 18:04:26 -0500

    update Problem 1176 in vscode

--- Easy/Diet Plan Performance.py
+++ Easy/Diet Plan Performance.py
@@ -5,17 +5,22 @@ def dietPlanPerformance(calories: List[int], k: int, lower: int, upper: int) -> 
     currSum, currStart = 0, 0
     currCount = 0
     res = 0
-    for i in range(size-k):
+    i = 0
+    while i < size - 1:
         if currCount != k:
             currSum += calories[i]
             currCount += 1
+            print(currSum)
         if currCount == k:
             if currSum < lower:
                 res -= 1
             elif currSum > upper:
                 res += 1
-            currSum -= calories[currStart] - calories[i+1]
+            currSum -= calories[currStart]
+            currCount -= 1
             currStart += 1
+        i += 1
+    currSum += calories[i]
     if currSum < lower:
         res -= 1
     elif currSum > upper:
