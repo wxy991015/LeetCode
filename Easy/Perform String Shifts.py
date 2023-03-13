bedd3d991b926e9bfbb00545df049c2f9f9697b0
Xiaoyang Wei <wei.xiaoyang@wustl.edu>
2023-03-13 17:27:29 -0500

    solution of Problem 1427

--- Easy/Perform String Shifts.py
+++ Easy/Perform String Shifts.py
@@ -1,15 +1,27 @@
 from typing import List
 
+# version 1 - modulo and string slicing method
 def stringShift(s: str, shift: List[List[int]]) -> str:
+    def leftShift(s: str, steps: int) -> str:
+        while steps != 0:
+            s = s[1:] + s[0]
+            steps -= 1
+        return s
+    def rightShift(s: str, steps: int) -> str:
+        while steps != 0:
+            s = s[-1] + s[:len(s)-1]
+            steps -= 1
+        return s
     size = len(s)
-    listS = list(s)
     for i in range(len(shift)):
-        direction, amount = shift[i]
-        if direction == 1:
-            first = listS[0]
-            for j in range(0, size-1):
-                listS[j] = listS[j+1]
-            listS[-1] = first
+        direction, steps = shift[i]
+        steps %= size
+        if direction == 0:
+            s = leftShift(s, steps)
         else:
-            pass
-    
+            s = rightShift(s, steps)
+    return s
+
+s = "abcdefg"
+shift = [[1,1],[1,1],[0,2],[1,3]]
+print(f"Output: {stringShift(s, shift)}")
